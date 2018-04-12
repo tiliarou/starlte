dfadfff99843cfe0d744938b3488dc77ee371c52
Andrei F <luxneb@gmail.com>
2018-04-12 11:06:56 +0200

    sched: sync ehmp with new WALT and PELT changes
    Signed-off-by: FAROVITUS <farovitus@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1768,7 +1768,9 @@ static inline unsigned long __cpu_util(int cpu, int delta)
 		util = max_t(unsigned long, util, READ_ONCE(cfs_rq->avg.util_est.enqueued));
 	
 	util += cpu_rq(cpu)->rt.avg.util_avg;
+#ifdef CONFIG_SCHED_WALT
 util_walt:
+#endif
 	delta += util;
 	if (delta < 0)
 		return 0;
@@ -1799,7 +1801,9 @@ static inline unsigned long cpu_util_freq(int cpu)
 				READ_ONCE(cpu_rq(cpu)->cfs.avg.util_est.enqueued));
 	
 	util += cpu_rq(cpu)->rt.avg.util_avg;
+#ifdef CONFIG_SCHED_WALT
 util_walt:
+#endif
 
 	return (util >= capacity) ? capacity : util;
 }
